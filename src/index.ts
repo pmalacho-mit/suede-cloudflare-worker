6b6819d3759c80450771fbc7b4ab9da220f3bec6
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2025-11-26 18:02:16 +0000

    adding suede.sh root

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,31 @@ const cache = {
 	cacheEverything: true,
 } satisfies RequestInitCfProperties;
 
+const index = `<!DOCTYPE html>
+<html>
+<head>
+<title>suede.sh</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+</head>
+<body>
+<main class="container">
+<p>This is a proxy for <code>${base}</code>.</p>
+<p>You can request files here and optionally omit the <code>.sh</code> suffix.</p>
+<p><strong>Example:</strong> <code>curl https://suede.sh/utils/degit</code></p>
+<p>View available scripts at <a href="https://github.com/pmalacho-mit/suede/tree/main/scripts">github.com/pmalacho-mit/suede</a></p>
+</main>
+</body>
+</html>`
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
 		let { pathname } = url;
+
+		if (pathname === '/')
+			return new Response(index, {
+				headers: { 'Content-Type': 'text/html' },
+			});
 
 		if (!pathname.endsWith('.sh')) pathname += '.sh';
 
