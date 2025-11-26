194caedb4e57a933a51c9ba5307b1c021810069f
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2025-11-26 21:30:43 +0000

    updating suede.sh index

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,40 @@ const cache = {
 } satisfies RequestInitCfProperties;
 
 const index = `<!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>suede.sh</title>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>suede.sh</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
 </head>
 <body>
-<main class="container">
-<p>This is a proxy for <code>${base}</code>.</p>
-<p>You can request files here and optionally omit the <code>.sh</code> suffix.</p>
-<p><strong>Example:</strong> <code>curl https://suede.sh/utils/degit</code></p>
-<p>View available scripts at <a href="https://github.com/pmalacho-mit/suede/tree/main/scripts">github.com/pmalacho-mit/suede</a></p>
-</main>
+	<main class="container">
+		<h1>suede.sh</h1>
+		
+		<p>This service provides cached access to scripts from:</p>
+		<pre><code>${base}</code></pre>
+		
+		<p>Requests may omit the <code>.sh</code> file extension for convenience.</p>
+		
+		<h2>Example</h2>
+		<pre><code>curl https://suede.sh/utils/degit</code></pre>
+		
+		<p>Returns the content of:</p>
+		<pre><code>https://raw.githubusercontent.com/pmalacho-mit/suede/refs/heads/main/scripts/utils/degit.sh</code></pre>
+		
+		<hr>
+		
+		<p>
+			Browse available scripts at 
+			<a href="https://github.com/pmalacho-mit/suede/tree/main/scripts">github.com/pmalacho-mit/suede</a>
+		</p>
+		
+		<p>
+			Source code for this worker: 
+			<a href="https://github.com/pmalacho-mit/suede-cloudflare-worker">github.com/pmalacho-mit/suede-cloudflare-worker</a>
+		</p>
+	</main>
 </body>
 </html>`
 
